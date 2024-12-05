61f7cbbae2f8ab0cfac5dac66a4967fdefd4219b
numakuro-0xf00 <enheione1234@gmail.com>
2024-12-06 08:18:29 +0900

    reIntall tailwindcss postcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
@@ -8,7 +6,7 @@ export function App() {
 
   return (
     <>
-      <header class= "sticky top-0 bg-white border-b border-gray-300">
+      <header className="sticky top-0 border-b border-gray-300">
         <div class="flex items-center justify-between px-3 sm:px-6 lg:px-5">
           <div class="flex items-center">
             <a href="/">
