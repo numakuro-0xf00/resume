8aa9a5218e80a08bc86cbc29e2860928c796a387
numakuro-0xf00 <enheione1234@gmail.com>
2024-12-05 22:06:31 +0900

    ヘッダーのタグを作成しました

--- src/app.tsx
+++ src/app.tsx
@@ -8,36 +8,15 @@ export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <header class= "sticky top-0 bg-white border-b border-gray-300">
+        <div class="flex items-center justify-between px-3 sm:px-6 lg:px-5">
+          <div class="flex items-center">
+            <a href="/">
+              <p>Hiroyuki Akanuma</p>
+            </a>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
