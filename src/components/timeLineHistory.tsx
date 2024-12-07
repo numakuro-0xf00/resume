f283f664f106a34ac61c539c29ad5cf10b32397d
numakuro-0xf00 <enheione1234@gmail.com>
2024-12-07 22:45:25 +0900

    install types/styled-components

---
+++ src/components/timeLineHistory.tsx
@@ -0,0 +1,16 @@
+
+import styled from 'styled-components';
+
+const TimeLineHistory = ({}) => {
+    return (
+        <SUncountedList className="timeline">
+
+        </SUncountedList>
+    );
+};
+
+const SUncountedList = styled.ul`
+    list-style-type: dotted;
+    padding: 0;
+`;
+
